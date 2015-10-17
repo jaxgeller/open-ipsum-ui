18c5708a0df0262af233e259e78796d6cd5b04dc
jaxgeller <jacksongeller@gmail.com>
2015-10-17 17:38:05 -0400

    add signup

--- app/components/sign-up.js
+++ app/components/sign-up.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  ajax: Ember.inject.service(),
+  session: Ember.inject.service(),
+  actions: {
+    signup() {
+      let u = {
+        username: this.get('username'),
+        email: this.get('email'),
+        password: this.get('password'),
+        password_confirmation: this.get('password_confirmation')
+      }
+
+      this.get('ajax')
+        .request('http://localhost:3000/users', 'POST', {data: {user: u}}).then(res => {
+          this.get('session').setToken(res.token);
+        });
+
+    }
+  }
 });
