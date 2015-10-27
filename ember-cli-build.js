eebe43b4347f929b82a982d4e18cfa78d4fb020d
jaxgeller <jacksongeller@gmail.com>
2015-10-27 13:26:19 -0400

    add all npm dependencies. update ember build system.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,9 +1,16 @@
 /*jshint node:true*/
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var autoprefixer = require('autoprefixer');
+var lost = require('lost');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
+    postcssOptions: {
+      plugins: [ { module: autoprefixer, options: {browsers: ['last 2 version']} }, {module: lost} ],
+      useSass: true,
+      extension: 'scss'
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
