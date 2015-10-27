cad1e68fa8a65a54c27d4b5be752b2c0f3f35514
jaxgeller <jacksongeller@gmail.com>
2015-10-27 19:27:34 -0400

    refactor session handlers

--- app/services/session.js
+++ app/services/session.js
@@ -3,12 +3,6 @@ import Ember from 'ember';
 export default Ember.Service.extend({
   token: localStorage.getItem('token'),
 
-
-  getAuthorization() {
-    let token = `${this.get('token')}:`;
-    return {'Authorization': `Basic ${btoa(token)}`}
-  },
-
   setToken(token) {
     this.set('token', token);
     localStorage.setItem('token', token);
@@ -17,5 +11,10 @@ export default Ember.Service.extend({
   removeToken() {
     this.set('token', null);
     localStorage.clear();
+  },
+
+  getAuthorization() {
+    let token = `${this.get('token')}:`;
+    return {'Authorization': `Basic ${btoa(token)}`}
   }
 });
