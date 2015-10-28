85432a2f1737b41303c2507155da0b792a94dd5b
jaxgeller <jacksongeller@gmail.com>
2015-10-27 20:11:10 -0400

    add copy to clipboard

--- app/components/ipsums/show-ipsum.js
+++ app/components/ipsums/show-ipsum.js
@@ -1,5 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  copyStatus: 'Copy to clipboard',
 
+  init() {
+    this._super();
+    new Clipboard('.topbar-button');
+  },
+
+  actions: {
+    click() {
+      this.set('copyStatus', 'Copied!');
+    }
+  }
 });
---
+++ app/helpers/copy-text.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+
+export function copyText(params/*, hash*/) {
+  return params;
+}
+
+export default Ember.Helper.helper(copyText);
