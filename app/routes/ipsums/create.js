ccd433f3e6d2351dbce7bb12407b0fc53fd36e9e
jaxgeller <jacksongeller@gmail.com>
2015-11-03 10:12:38 -0500

    fix tests

--- app/routes/ipsums/create.js
+++ app/routes/ipsums/create.js
@@ -1,11 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  api: Ember.inject().service(),
-
-  actions: {
-    createIpsum() {
-
-    }
-  }
 });
--- app/routes/ipsums/show.js
+++ app/routes/ipsums/show.js
@@ -1,9 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  api: Ember.inject.service(),
-
-  model(params) {
-    return this.get('api').request(`/ipsums/${params.id}`);
-  }
 });
