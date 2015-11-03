605ee35495c1374f25a9150cd15fea4c9eb59f13
jaxgeller <jacksongeller@gmail.com>
2015-11-02 19:08:44 -0500

    add init routes

--- app/router.js
+++ app/router.js
@@ -6,6 +6,19 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('browse');
+
+  this.route('signup');
+  this.route('login');
+  this.route('logout');
+
+  this.route('account');
+
+  this.route('ipsums', function() {
+    this.route('show', {path: '/:id'});
+    this.route('edit');
+    this.route('create', {path: '/create'});
+  });
 });
 
 export default Router;
---
+++ tests/unit/routes/ipsums/show-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:ipsums/show', 'Unit | Route | ipsums/show', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
