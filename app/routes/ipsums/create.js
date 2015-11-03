cdaba00e11095cc3b12c42e65f660d62736cfb01
jaxgeller <jacksongeller@gmail.com>
2015-11-03 11:56:34 -0500

    add authorization to create ipsums

--- app/routes/ipsums/create.js
+++ app/routes/ipsums/create.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import needsAuthorization from 'open-ipsum-ui/mixins/needs-authorization';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(needsAuthorization, {
 });
