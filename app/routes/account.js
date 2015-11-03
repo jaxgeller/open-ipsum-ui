3afb520d847630e92f4909085008e6853f6f4d83
jaxgeller <jacksongeller@gmail.com>
2015-11-03 17:11:05 -0500

    change up testing methods

--- app/routes/account.js
+++ app/routes/account.js
@@ -2,4 +2,9 @@ import Ember from 'ember';
 import needsAuthorization from 'open-ipsum-ui/mixins/needs-authorization';
 
 export default Ember.Route.extend(needsAuthorization, {
+  session: Ember.inject.service(),
+  api: Ember.inject.service(),
+  model() {
+    return this.get('api').authenticated(`/users/${this.get('session.username')}`);
+  }
 });
--- app/services/session.js
+++ app/services/session.js
@@ -1,22 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  username: '',
+  token: '',
   loggedIn: localStorage.getItem('token') || false,
 
   save(data) {
     localStorage.setItem('token', data.token);
     localStorage.setItem('username', data.username);
+    this.set('username', data.username);
+    this.set('token', data.token);
     this.set('loggedIn', true);
   },
 
   delete() {
     localStorage.removeItem('token');
     localStorage.removeItem('username');
+    this.set('username', null);
+    this.set('token', null);
     this.set('loggedIn', false);
   },
 
   authorize() {
-    let token = `${localStorage.getItem('token')}:`;
+    let token = `${this.get('token')}:`;
     return {'Authorization': `Basic ${btoa(token)}`};
   }
 });
