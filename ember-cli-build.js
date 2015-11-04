9636f674c3a4722d91496cd0b9defde23901cd40
jaxgeller <jacksongeller@gmail.com>
2015-11-03 20:45:59 -0500

    add some styls

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,11 +1,18 @@
 /*jshint node:true*/
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var autoprefixer = require('autoprefixer');
+var lost = require('lost');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    postcssOptions: {
+      plugins: [ { module: autoprefixer, options: {browsers: ['last 2 version']} }, {module: lost} ],
+      useSass: true,
+      extension: 'scss'
+    }
   });
+
 
   return app.toTree();
 };
