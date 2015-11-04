b789dece48fc9702cd91c75370969aa302fd19f7
jaxgeller <jacksongeller@gmail.com>
2015-11-03 19:31:10 -0500

    adding ipsums actions

--- app/routes/account.js
+++ app/routes/account.js
@@ -4,6 +4,7 @@ import needsAuthorization from 'open-ipsum-ui/mixins/needs-authorization';
 export default Ember.Route.extend(needsAuthorization, {
   session: Ember.inject.service(),
   api: Ember.inject.service(),
+
   model() {
     return this.get('api').authenticated(`/users/${this.get('session.username')}`);
   }
