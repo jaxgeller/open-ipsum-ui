656cec9b74ecc25ec0b935bc55f07ad9a9d7b6fc
jaxgeller <jacksongeller@gmail.com>
2015-12-15 17:25:51 -0500

    get jshint to pass routes

--- app/routes/not-found.js
+++ app/routes/not-found.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   beforeModel() {
-    this.transitionTo('ipsums.show', '404-ipsum')
+    this.transitionTo('ipsums.show', '404-ipsum');
   }
 });
