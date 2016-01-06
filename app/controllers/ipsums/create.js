09c7952a9dd9d80ca603ab18838e43e13057f0ab
jaxgeller <jacksongeller@gmail.com>
2016-01-05 22:43:04 -0500

    fix newline commaify bug #49

--- app/controllers/ipsums/create.js
+++ app/controllers/ipsums/create.js
@@ -18,7 +18,7 @@ export default Ember.Controller.extend({
   actions: {
     commaify() {
       let text = this.get('model.ipsum.text');
-      this.set('model.ipsum.text', text.replace(/[ ,]+/g, ", "));
+      this.set('model.ipsum.text', text.replace(/[ ,]+|\n+/g, ", "));
     },
 
     create() {
